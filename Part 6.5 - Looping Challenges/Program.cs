ffad0f098b6faae011352e508bf4e2b5636bd1c5
Judah Godden <339633265@GoTVDSB.ca>
2024-09-20 09:05:53 -0400

    Add project files.

---
+++ Part 6.5 - Looping Challenges/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_6._5___Looping_Challenges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                { 
+                    for (int k = i; k > 0; k--)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(j); 
+                }
+                Console.Write("\n");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
